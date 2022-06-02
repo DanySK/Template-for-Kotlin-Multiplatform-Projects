2770c65f7d5e52f0a10a28bca5afbdfa461a170f
Danilo Pianini <danilo.pianini@gmail.com>
2022-06-02 16:30:19 +0200

    build(macos): support mac os x with x86_64 arch

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,7 +61,7 @@ kotlin {
             "linux" to "mips", "linux" to "mips32" -> ::linuxMips32
             "linux" to "mipsel", "linux" to "mips32el" -> ::linuxMipsel32
             "mac os x" to "aarch64" -> ::macosArm64
-            "mac os x" to "amd64" -> ::macosX64
+            "mac os x" to "amd64", "mac os x" to "x86_64" -> ::macosX64
             "windows" to "amd64" -> ::mingwX64
             "windows" to "x86" -> ::mingwX86
             else -> throw GradleException("Host OS $hostOs with arch $hostArch is not supported in Kotlin/Native.")
