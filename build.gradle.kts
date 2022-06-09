26c3683bc9257a2baa90ff0d67ae3f0dffceb5ed
Danilo Pianini <danilo.pianini@unibo.it>
2022-06-09 18:03:21 +0200

    build: add support for windows server 2022

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ kotlin {
             "linux" to "mipsel", "linux" to "mips32el" -> ::linuxMipsel32
             "mac os x" to "aarch64" -> ::macosArm64
             "mac os x" to "amd64", "mac os x" to "x86_64" -> ::macosX64
-            "windows" to "amd64" -> ::mingwX64
+            "windows" to "amd64", "windows server 2022" to "amd64" -> ::mingwX64
             "windows" to "x86" -> ::mingwX86
             else -> throw GradleException("Host OS $hostOs with arch $hostArch is not supported in Kotlin/Native.")
         }
