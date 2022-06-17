e11024c9ba3a48a37cc07124e36d953c4a0743e3
Danilo Pianini <danilo.pianini@unibo.it>
2022-06-17 18:01:35 +0200

    fix(release): fix publication on Central

--- release.config.js
+++ release.config.js
@@ -1,14 +1,9 @@
 var publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 1
 git push --force origin \${nextRelease.version} || exit 2
-./gradlew releaseKotlinMultiplatformOnMavenCentralNexus || exit 3
-./gradlew releaseJvmOnMavenCentralNexus || exit 4
-./gradlew releaseJsOnMavenCentralNexus || exit 5
-./gradlew releaseNativeOnMavenCentralNexus || exit 6
-./gradlew releaseWasm32OnMavenCentralNexus || exit 7
-./gradlew releaseKotlinMavenOnMavenCentralNexus || exit 8
-./gradlew publishJsPackageToNpmjsRegistry || exit 9
-./gradlew publishKotlinMavenPublicationToGithubRepository || true
+./gradlew uploadAllPublicationsToMavenCentralNexus releaseStagingRepositoryOnMavenCentral || exit 3
+./gradlew publishJsPackageToNpmjsRegistry || exit 4
+./gradlew publishKotlinMultiplatformPublicationToGithubRepository || true
 `
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
