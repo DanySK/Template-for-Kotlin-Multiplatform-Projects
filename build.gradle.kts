9a62eef807d2474bee8bfb15e1dd370de46c04b1
Danilo Pianini <danilo.pianini@unibo.it>
2023-11-06 14:22:35 +0100

    chore(deps): update plugin kotlin-qa to v0.54.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import org.danilopianini.gradle.mavencentral.JavadocJar
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.gradle.internal.os.OperatingSystem
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
