c27ca2b723040f6160a4fe27d440fdffa4486dd2
Danilo Pianini <danilo.pianini@gmail.com>
2024-11-05 11:48:18 +0100

    ci(renovate):  use `with` when importing the configuration instead of `assert`

--- release.config.mjs
+++ release.config.mjs
@@ -2,7 +2,7 @@ const publishCmd = `
 ./gradlew -PstagingRepositoryId=\${process.env.STAGING_REPO_ID} releaseStagingRepositoryOnMavenCentral || exit 3
 ./gradlew publishJsPackageToNpmjsRegistry || exit 4
 `
-import config from 'semantic-release-preconfigured-conventional-commits' assert { type: "json" };
+import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
 config.plugins.push(
     [
         "@semantic-release/exec",
